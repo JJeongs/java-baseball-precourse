65957556b1bfbccd610501f7f39c52be7ee609c2
소정 <zzeong9@gmail.com>
2022-09-28 22:54:52 +0900

    feat: "숫자를 입력해주세요 : " 출력 및 사용자 입력 받기

---
+++ src/main/java/baseball/BaseballView.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseballView {
+    public void startGame() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String number = Console.readLine();
+    }
+}
