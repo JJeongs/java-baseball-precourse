4bc7e4f21a83df89676eaa67124bb004374d933a
소정 <zzeong9@gmail.com>
2022-09-30 00:44:26 +0900

    feat: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요." 출력 및 사용자 입력 받기
    - 1 입력: "숫자를 입력해주세요 : " 출력 및 사용자 입력 받기부터 시작
    - 2 입력: 애플리케이션 종료
    - 그 외 입력: `IllegalArgumentException`을 발생시킨 후 애플리케이션 종료

--- src/main/java/baseball/BaseballController.java
+++ src/main/java/baseball/BaseballController.java
@@ -1,6 +1,10 @@
 package baseball;
 
 public class BaseballController {
+    public static final boolean GAME_IS_ENDED = true;
+    public static final boolean GAME_IS_NOT_ENDED = false;
+    public static final String RESTART_GAME = "1";
+    public static final String END_GAME = "2";
     BaseballService baseballService;
 
     public BaseballController(BaseballService baseballService) {
@@ -13,11 +17,22 @@ public class BaseballController {
         System.out.println(score);
         if (baseballService.isGameEnded(score)) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return GAME_IS_ENDED;
         }
-        return true;
+        return GAME_IS_NOT_ENDED;
     }
 
     public String makeAnswer() {
         return baseballService.generateRandomThreeDigitNumber();
+    }
+
+    public boolean checkRestart(String input) {
+        if (input.equals(RESTART_GAME)) {
+            return true;
+        }
+        if (input.equals(END_GAME)) {
+            return false;
+        }
+        throw new IllegalArgumentException();
     }
 }
--- src/main/java/baseball/BaseballView.java
+++ src/main/java/baseball/BaseballView.java
@@ -17,5 +17,14 @@ public class BaseballView {
             String input = Console.readLine();
             isFinished = baseballController.playGame(answer, input);
         }
+        endGame();
+    }
+
+    private void endGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        if (baseballController.checkRestart(input)) {
+            startGame();
+        }
     }
 }
